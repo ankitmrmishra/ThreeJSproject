7a7388ce3a0175be05d07a41bef3a4c78a04d608
ANKIT MISHRA <ankitmrmishra1118@gmail.com>
2022-12-09 16:57:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
- import * as THREE from 'fileUrl';
+import * as THREE from '../build/three.module.js';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
 import { Scene } from 'three';
